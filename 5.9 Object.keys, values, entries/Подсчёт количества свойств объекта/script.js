934366452f9c7768a51d32cdb6e66d843f4500ad
Tim <64282785+TuM0IIIA@users.noreply.github.com>
2023-03-01 00:54:43 +0300

    Add files via upload

---
+++ 5.9 Object.keys, values, entries/Подсчёт количества свойств объекта/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function count(obj) {
+    return Object.keys(obj).length;
+    }
+    
+
+let user = {
+    name: 'John',
+    age: 30
+    };
+
+alert( count(user) ); // 2
+    
---
+++ 5.9 Object.keys, values, entries/Сумма свойств объекта/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function sumSalaries(salaries) {   
+    return Object.values(salaries).reduce((a, b) => a + b, 0) //via reduce     
+    };
+    
+let salaries = {
+"John": 100,
+"Pete": 300,
+"Mary": 250
+};
+
+alert( sumSalaries(salaries) ); // 650
+
+
+
+
+
+
